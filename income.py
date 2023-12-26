97de646280897d6cde15a8371e7925f1b7b76d77
chappellh1 <chappellh@sou.edu>
2023-12-26 13:27:06 -0800

    sql lite version

--- income.py
+++ income.py
@@ -1,14 +1,38 @@
+import sqlite3
+
 class Income:
-    def __init__(self, amount, date, source, description):
+    def __init__(self, amount=0.0, timeframe='', state='', source=''):
         self.amount = amount
-        self.date = date
+        self.timeframe = timeframe
+        self.state = state
         self.source = source
-        self.description = description
-    
-    def get_income_from_user():
-        print("Enter income details:")
-        amount = float(input("Amount: "))
-        date = input("Date (YYYY-MM-DD): ")
-        source = input("Source: ")
-        description = input("Description: ")
-        return Income(amount, date, source, description)
+
+    # ... [other methods] ...
+
+    def save_to_db(self, db_path):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        
+        # Create table if it doesn't exist
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS income (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                amount REAL,
+                timeframe TEXT,
+                state TEXT,
+                source TEXT
+            )
+        ''')
+
+        # Insert data
+        cursor.execute('''
+            INSERT INTO income (amount, timeframe, state, source) 
+            VALUES (?, ?, ?, ?)
+        ''', (self.amount, self.timeframe, self.state, self.source))
+
+        conn.commit()
+        conn.close()
+
+# Example usage (outside the Income class)
+my_income = Income(5000, 'Monthly', 'CA', 'Software Engineer')
+my_income.save_to_db('my_finances.db')
--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-from expense import Expense
-from budget import Budget
-from incometracker import Incometracker
+# main.py
+from income import Income
 
 def main():
-    # Sample data
-    expense1 = Expense(50, "2023-12-01", "groceries", "Supermarket")
-    
+    print("Welcome to the Financial and Budget Tracker")
 
-    income_tracker = Incometracker()
-    
-    while True:
-        income = income_tracker.get_income_from_user()
-        income_tracker.add_income(income)
-        another = input("Add another income? (yes/no): ")
-        if another.lower() != 'yes':
-            break
+    # Get user input for income details
+    amount = float(input("Enter the income amount: "))
+    timeframe = input("Enter the income timeframe (e.g., Monthly, Weekly): ")
+    state = input("Enter the state where the income was earned: ")
+    source = input("Enter the source of the income (e.g., Job Title): ")
 
+    # Create an Income object
+    my_income = Income(amount, timeframe, state, source)
 
-    total = income_tracker.total_income()
-    print(f"Total Income: {total}")
+    # Save the income to the database
+    db_path = 'my_finances.db'
+    my_income.save_to_db(db_path)
 
-    # Create a budget and add an expense
-    food_budget = Budget("groceries", 300)
-    food_budget.add_expense(expense1)
-
-    # Display budget details
-    print(f"Budget for {food_budget.category}: {food_budget.limit}")
-    print(f"Current total: {food_budget.current_total()}")
-
+    print("Income saved successfully.")
 
 if __name__ == "__main__":
     main()
